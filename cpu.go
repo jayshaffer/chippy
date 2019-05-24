bd2af10aa3f1878cf20876872b12c61931075863
Jay Shaffer <shaffersan@gmail.com>
2019-05-23 23:09:20 -0600

    Adding boot and last ops

--- cpu.go
+++ cpu.go
@@ -2,6 +2,8 @@ package chippy
 
 import (
 	"github.com/golang-collections/collections/stack"
+	"math/rand"
+	"time"
 )
 
 type CPU struct {
@@ -10,6 +12,120 @@ type CPU struct {
 	registers map[uint8]uint8
 	vf        bool
 	i         uint16
+	sp        uint8
+	dt        uint8
+	st        uint8
+}
+
+func boot() {
+	cpu := new(CPU)
+	cpu.pc = 0
+	cpu.vf = false
+	cpu.i = 0
+	cpu.sp = 0
+	cpu.dt = 0
+	cpu.st = 0
+	cpu.registers = map[uint8]uint8{
+		0x00: 0,
+		0x01: 0,
+		0x02: 0,
+		0x03: 0,
+		0x04: 0,
+		0x05: 0,
+		0x06: 0,
+		0x07: 0,
+		0x08: 0,
+		0x09: 0,
+		0x0A: 0,
+		0x0B: 0,
+		0x0C: 0,
+		0x0D: 0,
+		0x0E: 0,
+		0x0F: 0}
+}
+
+func (cpu CPU) command(instruction uint16) {
+	masked := instruction & 0xf000
+	switch masked {
+	case 0x0000:
+		switch instruction {
+		case 0x00e0:
+			cpu.CLS(instruction)
+		case 0x00ee:
+			cpu.RET(instruction)
+		}
+	case 0x1000:
+		cpu.JP(instruction)
+	case 0x2000:
+		cpu.CALL(instruction)
+	case 0x3000:
+		cpu.SEKK(instruction)
+	case 0x4000:
+		cpu.SNE(instruction)
+	case 0x5000:
+		cpu.SEVY(instruction)
+	case 0x6000:
+		cpu.LD(instruction)
+	case 0x7000:
+		cpu.ADDKK(instruction)
+	case 0x8000:
+		switch command := instruction; {
+		case (command & 0x8001) == 0x8001:
+			cpu.OR(instruction)
+		case (command & 0x8002) == 0x8002:
+			cpu.AND(instruction)
+		case (command & 0x8003) == 0x8003:
+			cpu.XOR(instruction)
+		case (command & 0x8004) == 0x8004:
+			cpu.ADDVY(instruction)
+		case (command & 0x8005) == 0x8005:
+			cpu.SUB(instruction)
+		case (command & 0x8006) == 0x8006:
+			cpu.SHR(instruction)
+		case (command & 0x8007) == 0x8007:
+			cpu.SUBN(instruction)
+		case (command & 0x800e) == 0x800e:
+			cpu.SHL(instruction)
+		}
+	case 0x9000:
+		cpu.SNE(instruction)
+	case 0xA000:
+		cpu.LDI(instruction)
+	case 0xB000:
+		cpu.JP0(instruction)
+	case 0xC000:
+		cpu.RND(instruction)
+	case 0xD000:
+		cpu.DRW(instruction)
+	case 0xE000:
+		switch command := instruction; {
+		case (command & 0xe09e) == 0xe090:
+			cpu.SKP(instruction)
+		case (command & 0xe0a1) == 0xe0a1:
+			cpu.SKNP(instruction)
+		}
+	case 0xF000:
+		switch command := instruction; {
+		case (command & 0xf007) == 0xf007:
+			cpu.LD_VX_DT(instruction)
+		case (command & 0xf00A) == 0xf00A:
+			cpu.LD_VX_K(instruction)
+		case (command & 0xf015) == 0xf015:
+			cpu.LDDT(instruction)
+		case (command & 0xf018) == 0xf018:
+			cpu.LDST(instruction)
+		case (command & 0xf01E) == 0xf01E:
+			cpu.ADDI(instruction)
+		case (command & 0xf029) == 0xf029:
+			cpu.LDF(instruction)
+		case (command & 0xf033) == 0xf033:
+			cpu.LDB(instruction)
+		case (command & 0xf055) == 0xf055:
+			cpu.LDIVX(instruction)
+		case (command & 0xf065) == 0xf065:
+			cpu.LDVXI(instruction)
+		}
+	}
 }
 
 func (cpu CPU) SYS(instruction uint16) {
@@ -103,7 +219,7 @@ func (cpu *CPU) SHL(instruction uint16) {
 	cpu.setRegisterFromVx(instruction, vx<<2)
 }
 
-func (cpu *CPU) SNE(instruction uint16) {
+func (cpu *CPU) SNEVY(instruction uint16) {
 	vx := cpu.getVx(instruction)
 	vy := cpu.getVy(instruction)
 	if vx != vy {
@@ -111,11 +227,66 @@ func (cpu *CPU) SNE(instruction uint16) {
 	}
 }
 
-func (cpu *CPU) LD(instruction uint16) {
+func (cpu *CPU) LDI(instruction uint16) {
+	cpu.i = instruction & 0x0fff
 }
 
-func (cpu *CPU) LD(instruction uint16) {
-	cpu.i = instruction & 0x0fff
+func (cpu *CPU) JP0(instruction uint16) {
+	cpu.pc = instruction&0x0fff + uint16(cpu.registers[0])
+}
+
+func (cpu *CPU) DRW(instruction uint16) {
+	//show sprite
+}
+
+func (cpu *CPU) SKP(instruction uint16) {
+	//Skip if reg matched key is pressed
+}
+
+func (cpu *CPU) SKNP(instruction uint16) {
+	//Skip if reg matched key is not pressed
+}
+
+func (cpu *CPU) LD_VX_DT(instruction uint16) {
+	cpu.setRegisterFromVx(instruction, cpu.dt)
+}
+
+func (cpu *CPU) LDDT(instruction uint16) {
+	cpu.dt = cpu.getVx(instruction)
+}
+
+func (cpu *CPU) LD_VX_K(instruction uint16) {
+	//Wait for a key press, store the value of the key in Vx.
+}
+
+func (cpu *CPU) LDST(instruction uint16) {
+	cpu.st = cpu.getVx(instruction)
+}
+
+func (cpu *CPU) ADDI(instruction uint16) {
+	cpu.i = cpu.i + uint16(cpu.getVx(instruction))
+}
+
+func (cpu *CPU) LDF(instruction uint16) {
+	//Set I = location of sprite for digit Vx.
+}
+
+func (cpu *CPU) LDB(instruction uint16) {
+	//Store BCD representation of Vx in memory locations I, I+1, and I+2.
+}
+
+func (cpu *CPU) LDIVX(instruction uint16) {
+	//Store registers V0 through Vx in memory starting at location I.
+}
+
+func (cpu *CPU) LDVXI(instruction uint16) {
+	//Read registers V0 through Vx from memory starting at location I.
+}
+
+func (cpu *CPU) RND(instruction uint16) {
+	seed := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(seed)
+	cpu.setRegisterFromVx(instruction, uint8(r.Intn(256)))
 }
 
 func (cpu *CPU) SUBN(instruction uint16) {
